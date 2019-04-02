61eb164a352784f0789a55d427a7fd0680de966c
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-04-02 23:00:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 
  client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefixbc = "+";
-    if(message.content.startsWith(prefixbc + 'bc')) {
+    var prefix = "+";
+    if(message.content.startsWith(prefix + 'bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefixbc.length);
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
     let copy = "alpha codes";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('```**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**```');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
@@ -45,14 +45,14 @@ const client = new Discord.Client();
     });
 
 
-var prefixcl = '+';
+
 
 client.on('message', msg => {
 
   if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefixcl)) return;
+  if (!msg.content.startsWith(prefix)) return;
   let command = msg.content.split(" ")[0];
-  command = command.slice(prefixcl.length);
+  command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
  
     if(command === "clear") {
@@ -75,7 +75,7 @@ client.on('message', msg => {
 
 
 client.on('message', function(msg) {
-        var prefixm = '+';
+      
     if(msg.content.startsWith (prefixm + 'stats')) {
       if(!msg.channel.guild) return msg.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');
       let embed = new Discord.RichEmbed()
