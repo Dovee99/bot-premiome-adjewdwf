3e009840d970bdc38fd118affb160b02869379b8
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-04-05 00:08:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,9 +98,13 @@ client.on('message', function(msg) {
   });
 
 
-
-
-
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
  
  
  
